5a9beeb4edc7da0dbac985e285ff88964b0844c9
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-17 20:19:42 +0100

    Update finish.sh

--- data/finish.sh
+++ data/finish.sh
@@ -7,9 +7,9 @@ domain=`cat /etc/domain/d-domain`
 flare=`cat /etc/domain/f-domain`
 
 clear
-echo -e "\033[1;96m──────────────────────────────────\033[0m"
+echo -e "\033[1;95m──────────────────────────────────\033[0m"
 echo -e "\033[1;97m         SCRIPT BY ILYASS\033[0m"
-echo -e "\033[1;96m──────────────────────────────────\033[0m"
+echo -e "\033[1;95m──────────────────────────────────\033[0m"
 echo -e "\033[1;97mIP               : $IP\033[0m"
 echo -e "\033[1;97mHost             : $domain\033[0m"
 echo -e "\033[1;97mCloudflare       : $flare\033[0m"
@@ -17,7 +17,7 @@ echo -e "\033[1;97mPort OVPN TCP    : 1194\033[0m"
 echo -e "\033[1;97mPort OVPN UDP    : 2200\033[0m"
 echo -e "\033[1;97mPort OVPN WS     : 80\033[0m"
 echo -e "\033[1;97mPort OVPN SSL    : 443\033[0m"
-echo -e "\033[1;96m──────────────────────────────────\033[0m"
+echo -e "\033[1;95m──────────────────────────────────\033[0m"
 echo "0 4 * * * /sbin/shutdown -r +15" >> /var/spool/cron/crontabs/root
 echo
 echo
