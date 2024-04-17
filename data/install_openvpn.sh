c6c852f7c80570bc7b0ca4f2c05423fe44a39498
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-17 17:07:49 +0100

    Update install_openvpn.sh

--- data/install_openvpn.sh
+++ data/install_openvpn.sh
@@ -102,7 +102,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/FasterExE/FREE-Autoscript/main/ssh/vpn.zip
+wget https://github.com/FasterExE/OVPN-Script-V3/raw/main/data/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
